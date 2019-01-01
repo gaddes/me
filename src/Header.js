60dfb3c86b0229b486dd5dc996474c4a8f9a0c78
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-01-01 18:28:09 +0000

    component lifecycle methods in header.

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
+  componentDidUpdate() {
+    console.log("Header updated!");
+  }
+
+  componentDidMount() {
+    console.log("Header did mount!");
+  }
 
   render() {
     return (
@@ -8,9 +15,9 @@ class Header extends Component {
         <p>HEADER</p>
         <div className="buttons">
           {/* <button onClick={this.goToUrl('home')}>Home</button> */}
-          <button onClick={() => this.props.goToUrl('home')}>Home</button>
-          <button onClick={() => this.props.goToUrl('about')}>About</button>
-          <button onClick={() => this.props.goToUrl('contact')}>Contact</button>
+          <button onClick={() => this.props.goToUrl("home")}>Home</button>
+          <button onClick={() => this.props.goToUrl("about")}>About</button>
+          <button onClick={() => this.props.goToUrl("contact")}>Contact</button>
         </div>
       </div>
     );
