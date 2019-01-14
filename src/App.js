6175854240dcb0ba6d5f8b7e342c3cbfb264ced7
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-01-14 22:24:00 +0000

    Complete home page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Header from './Header';
 import Home from './Home';
 import './_reset.scss';
 
+// FontAwesome icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+library.add(faGithub);
+library.add(faLinkedinIn);
+library.add(faEnvelope);
+
 class App extends Component {
   render() {
     return (
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './home.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const myFace = require('./images/me.jpg');
 
 class Home extends Component {
@@ -14,7 +16,25 @@ class Home extends Component {
           <div className='myface-wrapper'>
             <img src={myFace} className='myface' alt="Matt's face" />
           </div>
-          <p className='text'>HOME</p>
+          <div className='contact-icons'>
+            <a className='icon' href='https://github.com/gaddes'>
+              <FontAwesomeIcon icon={['fab', 'github']} />
+            </a>
+            <a
+              className='icon'
+              href='https://www.linkedin.com/in/matthew-gaddes-30ba65b4/'
+            >
+              <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+            </a>
+            <a className='icon' href='mailto:matthew.gaddes@gmail.com'>
+              <FontAwesomeIcon icon={['far', 'envelope']} />
+            </a>
+          </div>
+          <div className='button-wrapper'>
+            <Link to='/contact' className='link'>
+              Contact me!
+            </Link>
+          </div>
         </div>
       </div>
     );
