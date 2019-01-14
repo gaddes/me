0d45a0da2a83a549851db0cd1cd92ffe6c4e1bc8
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-01-14 20:20:23 +0000

    Add header text and image to homepage.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
-import React, { Component } from "react";
-import "./home.scss";
+import React, { Component } from 'react';
+import './home.scss';
+const myFace = require('./images/me.jpg');
 
 class Home extends Component {
   render() {
     return (
-      <div className="body home">
-        <p>HOME</p>
+      <div className='body home'>
+        <div className='overlay-light'>
+          <p className='title'>Hi, I'm Matt!</p>
+          <p className='subtext'>
+            A Web Developer based in Newcastle-upon-Tyne
+          </p>
+          <div className='myface-wrapper'>
+            <img src={myFace} className='myface' alt="Matt's face" />
+          </div>
+          <p className='text'>HOME</p>
+        </div>
       </div>
     );
   }
