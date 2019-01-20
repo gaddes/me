4d8656f2b876dac7b8b8ee1bc077c5714410c59a
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-01-20 17:39:52 +0000

    Add images, urls and bullets for each project.

--- src/Project.js
+++ src/Project.js
@@ -6,7 +6,8 @@ const projectArray = [
   {
     title: 'Pasta Bake (TM)',
     alt: 'Pasta Bake (TM)',
-    image: require('./images/projects/pasta-bake_2.png'),
+    url: 'https://gaddes.github.io/PastaBake/',
+    image: require('./images/projects/pasta-bake.png'),
     bullets: [
       'this website is great because...',
       'it also includes this technology...',
@@ -14,9 +15,33 @@ const projectArray = [
     ]
   },
   {
-    title: 'Pasta Bake (TM)',
-    alt: 'Pasta Bake (TM)',
-    image: require('./images/projects/pasta-bake_2.png'),
+    title: 'Meldon Xmas',
+    alt: 'Meldon Xmas',
+    url: 'https://meldon-xmas.herokuapp.com/',
+    image: require('./images/projects/meldon-xmas.png'),
+    bullets: [
+      'this website is great because...',
+      'it also includes this technology...',
+      "here's a third bullet point just for the craic..."
+    ]
+  },
+  {
+    title: 'Matchless Design',
+    alt: 'Matchless Design',
+    url: 'http://matchlessdesign.co.uk/',
+    image: require('./images/projects/matchless-design.png'),
+    bullets: [
+      'this website is great because...',
+      'it also includes this technology...',
+      'Due to licensing issues, images in the gallery have been intentionally left blank'
+    ]
+  },
+  {
+    title: 'Coffee Companion',
+    alt: 'Coffee Companion',
+    url: '',
+    image: require('./images/projects/coffee-companion.png'),
+    video: require('./images/projects/coffee-companion.mp4'),
     bullets: [
       'this website is great because...',
       'it also includes this technology...',
@@ -31,17 +56,47 @@ const bulletPoints = projectArray.map(function(item) {
 });
 
 const projectObjects = projectArray.map(function(item) {
-  return (
-    <div className='project'>
-      <span className='title'>{item.title}</span>
+  // Only show video if it exists
+  let videoPath;
+  if (item.video) {
+    videoPath = (
+      <video className='video' loop src={item.video} autoPlay muted />
+    );
+  }
+
+  // Show <a> if there's an href, otherwise show <span>
+  let imagePath;
+  if (item.url) {
+    imagePath = (
       <a
         className='image-container'
-        href='https://gaddes.github.io/PastaBake/'
+        href={item.url}
         target='_blank'
         rel='noopener noreferrer'
       >
         <img className='image' src={item.image} alt={item.alt} />
       </a>
+    );
+  } else {
+    imagePath = (
+      <span
+        className='image-container'
+        href={item.url}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img className='image' src={item.image} alt={item.alt} />
+      </span>
+    );
+  }
+
+  return (
+    <div className='project'>
+      <span className='title'>{item.title}</span>
+      <div className='media-container'>
+        {imagePath}
+        {videoPath}
+      </div>
       <ul className='bullets'>
         {/* <li>{...item.bullets}</li> */}
         <li>{item.bullets[0]}</li>
